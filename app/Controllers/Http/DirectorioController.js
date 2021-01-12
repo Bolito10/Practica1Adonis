c909d1a038c8eaf86276d5ae874175c9d89fdea1
Bolito10 <luisitoibarra@live.com.mx>
2021-01-11 22:30:13 -0600

    Indexado total y parametros puestos

--- app/Controllers/Http/DirectorioController.js
+++ app/Controllers/Http/DirectorioController.js
@@ -1,92 +1,34 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+//const Directorio = require('../../Models/Directorio')
+const Directorio = use('App/Models/Directorio')
 
-/**
- * Resourceful controller for interacting with directorios
- */
+
 class DirectorioController {
-  /**
-   * Show a list of all directorios.
-   * GET directorios
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+  async index ({ request, response,})
+  {
+    return await Directorio.all();
   }
 
-  /**
-   * Render a form to be used for creating a new directorio.
-   * GET directorios/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store ({ request, response })
+  {
+
   }
 
-  /**
-   * Create/save a new directorio.
-   * POST directorios
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+
+  async show ({ params, request, response})
+  {
+    return await Directorio.findOrFail(params.id);
   }
 
-  /**
-   * Display a single directorio.
-   * GET directorios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+  async update ({ params, request, response })
+  {
+
   }
 
-  /**
-   * Render a form to update an existing directorio.
-   * GET directorios/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+  async destroy ({ params, request, response })
+  {
 
-  /**
-   * Update directorio details.
-   * PUT or PATCH directorios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a directorio with id.
-   * DELETE directorios/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,5 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+Route.resource('directorios', 'DirectorioController').apiOnly();
